8870dc34c98c77c6144591ea0d1556aefd35beca
Lukas Olson <lukasdragon@live.ca>
2024-02-29 11:55:51 -0800

    Initial files

---
+++ __init__.py
@@ -0,0 +1,15 @@
+"""
+A package of useful tools for photogrammetry processing.
+"""
+from strenum import StrEnum
+
+
+class ModelExecutionEngines(StrEnum):
+    """
+    An enum of common neural network execution engines.
+    """
+
+    CPU = "CPU"
+    CUDA = "CUDA"
+    TENSORRT = "TensorRT"
+    DirectML = "DirectML"
